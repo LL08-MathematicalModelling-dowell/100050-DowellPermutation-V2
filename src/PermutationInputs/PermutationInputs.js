ed20f0ecadbb1ebc3c67aca0796e1d85bb5e936a
Hamza-js <mhamzaawanmp1@gmail.com>
2023-12-04 12:22:28 +0500

    added logo

--- src/PermutationInputs/PermutationInputs.js
+++ src/PermutationInputs/PermutationInputs.js
@@ -1,83 +1,120 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch} from 'react-redux'
-import { assignSetN, assignSetR, assignPermutationResult, resetN, resetR, resetPermutationResult } from '../app/features/counter/CounterSlice'
-import './PermutationInputs.css'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import {
+  assignSetN,
+  assignSetR,
+  assignPermutationResult,
+  resetN,
+  resetR,
+  resetPermutationResult,
+} from "../app/features/counter/CounterSlice";
+import "./PermutationInputs.css";
 
 function PermutationInputs() {
+  const [n, setN] = React.useState("");
+  const [r, setR] = React.useState("");
+  const [nerror, setNError] = React.useState(false);
+  const [rerror, setRError] = React.useState(false);
+  const [showButtons, setShowButtons] = React.useState(false);
+  const [permutationResult, setPermutationResult] = React.useState(0);
+  const navigate = useNavigate();
 
-  const [n, setN]=React.useState('')
-  const [r, setR]=React.useState('')
-  const [nerror, setNError]=React.useState(false)
-  const [rerror, setRError]=React.useState(false)
-  const [showButtons, setShowButtons] = React.useState(false)
-  const [permutationResult, setPermutationResult] = React.useState(0)
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-
-  const calculatePermutation = (e) =>{
-    e.preventDefault()
-    if(n > 50 || n === ''){
-      setNError(!nerror)
-    }else if(r > 50 || r === ''){
-      setRError(!rerror)
-    }else{
+  const calculatePermutation = (e) => {
+    e.preventDefault();
+    if (n > 50 || n === "") {
+      setNError(!nerror);
+    } else if (r > 50 || r === "") {
+      setRError(!rerror);
+    } else {
       //Calculate permutation
-      setShowButtons(!showButtons)
-      let numerator = 1
-      let denominator = 1
-      for(let i=n; i>=1;i--){
-        numerator = numerator * i
+      setShowButtons(!showButtons);
+      let numerator = 1;
+      let denominator = 1;
+      for (let i = n; i >= 1; i--) {
+        numerator = numerator * i;
       }
 
-      for(let i=n-r; i>=1;i--){
-        denominator = denominator * i
+      for (let i = n - r; i >= 1; i--) {
+        denominator = denominator * i;
       }
-      setPermutationResult(numerator/denominator)
+      setPermutationResult(numerator / denominator);
     }
-  }
+  };
 
-  const handleShowPermutation = (e) =>{
-    e.preventDefault()
-    dispatch(assignSetN(n))
-    dispatch(assignSetR(r))
-    dispatch(assignPermutationResult(permutationResult))
-    navigate('/calculator')
-  }
+  const handleShowPermutation = (e) => {
+    e.preventDefault();
+    dispatch(assignSetN(n));
+    dispatch(assignSetR(r));
+    dispatch(assignPermutationResult(permutationResult));
+    navigate("/calculator");
+  };
 
-  const resetAll = () =>{
-    dispatch(resetN(''));
-    dispatch(resetR(''))
-    dispatch(resetPermutationResult(''))
-  }
+  const resetAll = () => {
+    dispatch(resetN(""));
+    dispatch(resetR(""));
+    dispatch(resetPermutationResult(""));
+  };
 
-  const handleReset = () =>{
-    setN(0)
-    setR(0)
-    resetAll()
-  }
+  const handleReset = () => {
+    setN(0);
+    setR(0);
+    resetAll();
+  };
 
   return (
-    <div className='permutationInputs'>
-      <h1>DoWell Permutations Calculator</h1>
+    <div className="permutationInputs">
       <form>
+        <img
+          className="logo"
+          src="https://www.uxlivinglab.org/wp-content/uploads/2023/10/image_1-3.png"
+          alt="logo"
+        />
+        <h1 className="heading">DoWell Permutations Calculator</h1>
+
         <label>Total amount in a set(n)</label>
-        <input type='number' id={nerror ? 'nInput':''} value={n} onChange={(e)=>setN(e.target.value)} max="50"/>
+        <input
+          type="number"
+          id={nerror ? "nInput" : ""}
+          value={n}
+          onChange={(e) => setN(e.target.value)}
+          max="50"
+        />
         <span>Hint: Max limit is 50</span>
         <label>Total amount in each subset(r)</label>
-        <input type='number' id={rerror ? 'rInput':''} value={r} onChange={(e)=>setR(e.target.value)}max="50"/>
-        <div className='permutationButtons' id={showButtons ? 'formButtons':''}>
-          <button className='resetButton' onClick={handleReset}>Reset</button>
-          <button className='calculateButton' onClick={calculatePermutation}>Calculate</button>
+        <input
+          type="number"
+          id={rerror ? "rInput" : ""}
+          value={r}
+          onChange={(e) => setR(e.target.value)}
+          max="50"
+        />
+        <div
+          className="permutationButtons"
+          id={showButtons ? "formButtons" : ""}
+        >
+          <button className="resetButton" onClick={handleReset}>
+            Reset
+          </button>
+          <button className="calculateButton" onClick={calculatePermutation}>
+            Calculate
+          </button>
         </div>
-        <div className='permutationResults' id={showButtons ? 'formResults':''}>
-          <p>Number of Permutations = <span className='amount'>{permutationResult}</span></p>
+        <div
+          className="permutationResults"
+          id={showButtons ? "formResults" : ""}
+        >
+          <p>
+            Number of Permutations ={" "}
+            <span className="amount">{permutationResult}</span>
+          </p>
           <button onClick={handleShowPermutation}>Show Permutations</button>
         </div>
       </form>
     </div>
-  )
+  );
 }
 
-export default PermutationInputs
+export default PermutationInputs;
